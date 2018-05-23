f20fa456362eda2fdd3642ea271f1901c29eab80
pqa <indigo.poirier@pqatesting.ca>
2018-05-23 16:27:11 -0300

    cat

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,5 +1,13 @@
 import UIKit
 
+import AppCenter
+import AppCenterPush
+import AppCenterAnalytics
+import AppCenterCrashes
+import AppCenterDistribute
+
+// 66861101-94f0-4419-a49e-b8f9162db292
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
@@ -9,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
+        MSAppCenter.start("66861101-94f0-4419-a49e-b8f9162db292", withServices:[MSDistribute.self, MSCrashes.self, MSAnalytics.self, MSPush.self])
         return true
     }
 
